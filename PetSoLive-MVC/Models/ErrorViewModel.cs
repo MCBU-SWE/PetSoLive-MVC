992f02133f591052f3d3c9af2ae3454e3600e740
Furkan Bulut <furkanbtng@gmail.com>
2024-12-20 15:38:15 +0300

    First Commit

---
+++ PetSoLive-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PetSoLive_MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
